77409f7f79b120e032e7c110bc8edb0d1d5193e7
Amya Moore <bookworm7572@gmail.com>
2025-01-02 12:42:05 -0500

    Layout Budget Page
    - started layout for budget page
    - added tables for salary, goals, and account contribution

--- src/app/pages/expense-tracker/budget/page.tsx
+++ src/app/pages/expense-tracker/budget/page.tsx
@@ -1,8 +1,28 @@
+import GoalsTable from "@/components/goals";
+import SalaryTable from "@/components/salary";
+import SavingsTable from "@/components/savings";
 
 export default function Budget() {
-	return(
-		<div className="col-span-4">
-			<h1>Budget</h1>
-		</div>
-	)
+
+  return (
+    <section
+      id="budget"
+      className="col-span-4 grid grid-cols-4 gap-4 md:col-span-6 md:grid-cols-6 lg:col-span-12 lg:grid-cols-12"
+    >
+      <div className="navigation col-span-4 bg-green-300 md:col-span-6 lg:col-span-2">
+        navigation
+      </div>
+
+      <main className="col-span-4 md:col-span-4 lg:col-span-8">
+        center content
+        <div className="date-wrapper">date</div>
+				<SalaryTable />
+				<GoalsTable />
+				<SavingsTable />
+      </main>
+      <div className="col-span-4 bg-blue-300 md:col-span-2 lg:col-span-2">
+        right side content
+      </div>
+    </section>
+  );
 }
---
+++ src/components/goals.tsx
@@ -0,0 +1,36 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+export default function GoalsTable() {
+  return (
+    <div className="goals-wrapper py-4">
+      <h2 className="mb-2">Goals</h2>
+      <TableContainer component={Paper}>
+        <Table id="goals-table" aria-label="Goals Table">
+          <TableHead className="bg-gray-300">
+            <TableRow>
+              <TableCell className="font-bold">Goal</TableCell>
+              <TableCell className="font-bold">Target Amount</TableCell>
+              <TableCell className="font-bold">Current Amount</TableCell>
+              <TableCell className="font-bold">Remaining</TableCell>
+              <TableCell className="font-bold">Progress</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* map here */}
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
