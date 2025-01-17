11c09d5685fc265c6b58b8dfaca17899b97de546
Amya Moore <bookworm7572@gmail.com>
2025-01-17 15:28:01 -0500

    Update page.tsx

--- src/app/pages/expense-tracker/dashboard/page.tsx
+++ src/app/pages/expense-tracker/dashboard/page.tsx
@@ -1,11 +1,4 @@
 "use client";
-import AccountTable from "@/components/accountTable";
-import ExpensesTable from "@/components/expensesTable";
-import GoalsTable from "@/components/goalsTable";
-import { SalaryTable } from "@/components/salaryTable";
-import TotalAssets from "@/components/totalAssets";
-import { Alert } from "@mui/material";
-import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   return <section id="dashboard"></section>;
