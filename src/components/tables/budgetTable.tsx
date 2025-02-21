64f9e1d023633ed4758d637293720acf84571387
Amya Moore <bookworm7572@gmail.com>
2025-02-21 09:50:12 -0500

    Added Tables and Table Styling

--- src/components/tables/budgetTable.tsx
+++ src/components/tables/budgetTable.tsx
@@ -1,62 +1,124 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function BudgetTable() {
-	return (
+  return (
     <div className="budget-table-wrapper">
       <TableContainer component={Paper} className="table-wrapper my-4">
         <Table aria-label="Budget Table" size="small" id="budget-table">
           <TableHead>
-            <TableRow>
-              <TableCell colSpan={5} className="font-bold">
+            <TableRow className="border-b-2 border-gray-300 bg-gray-200">
+              <TableCell className="font-bold md:hidden" colSpan={2}>
                 Budget Table
               </TableCell>
+              <TableCell className="hidden font-bold md:table-cell" colSpan={5}>
+                Budget Table
+              </TableCell>
+              <TableCell align="right">
+                <IconButton size="small">
+                  <AddIcon fontSize="small" />
+                </IconButton>
+              </TableCell>
             </TableRow>
-            <TableRow>
-              <TableCell className="font-bold">Category</TableCell>
-              <TableCell className="font-bold">% of Salary</TableCell>
-              <TableCell className="font-bold">Allotted Amount</TableCell>
-              <TableCell className="font-bold">Actual Spent</TableCell>
-              <TableCell className="font-bold">Remaining</TableCell>
+
+            <TableRow className="bg-gray-200">
+              <TableCell className="category font-bold">Category</TableCell>
+              <TableCell className="percentage hidden font-bold md:table-cell">
+                % of Salary
+              </TableCell>
+              <TableCell className="budgeted hidden font-bold md:table-cell">
+                Budgeted
+              </TableCell>
+              <TableCell className="spent hidden font-bold md:table-cell">
+                Spent
+              </TableCell>
+              <TableCell className="remaining font-bold">Remaining</TableCell>
+              <TableCell className="trash"></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell colSpan={5} className="font-bold">
+            <TableRow className="bg-gray-300">
+              <TableCell className="font-bold md:hidden" colSpan={3}>
+                Accounts - 50%
+              </TableCell>
+              <TableCell className="hidden font-bold md:table-cell" colSpan={6}>
                 Accounts - 50%
               </TableCell>
             </TableRow>
-            {/* TODO: map */}
+
+            {/* TODO: add map here */}
             <TableRow>
-              <TableCell className="italic">Savings Contributions</TableCell>
-              <TableCell>20%</TableCell>
-              <TableCell>$calc</TableCell>
-              <TableCell>$calc</TableCell>
-              <TableCell>$calc</TableCell>
+              <TableCell className="category italic">Something</TableCell>
+              <TableCell className="percentage hidden md:table-cell">
+                %
+              </TableCell>
+              <TableCell className="budgeted hidden md:table-cell">B</TableCell>
+              <TableCell className="spent hidden md:table-cell">S</TableCell>
+              <TableCell className="remaining">R</TableCell>
+              <TableCell className="trash" align="right">
+                <IconButton size="small">
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
+              </TableCell>
             </TableRow>
-            <TableRow>
-              <TableCell colSpan={5} className="font-bold">
+
+            <TableRow className="bg-gray-300">
+              <TableCell className="font-bold md:hidden" colSpan={3}>
+                Fixed Expenses - 30%
+              </TableCell>
+              <TableCell className="hidden font-bold md:table-cell" colSpan={6}>
                 Fixed Expenses - 30%
               </TableCell>
             </TableRow>
-            {/* TODO: map */}
+
+            {/* TODO: add map here */}
             <TableRow>
-              <TableCell className="italic">Needs</TableCell>
-              <TableCell>20%</TableCell>
-              <TableCell>$calc</TableCell>
-              <TableCell>$calc</TableCell>
-              <TableCell>$calc</TableCell>
+              <TableCell className="category italic">Something</TableCell>
+              <TableCell className="percentage hidden md:table-cell">
+                %
+              </TableCell>
+              <TableCell className="budgeted hidden md:table-cell">B</TableCell>
+              <TableCell className="spent hidden md:table-cell">S</TableCell>
+              <TableCell className="remaining">R</TableCell>
+              <TableCell className="trash" align="right">
+                <IconButton size="small">
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
+              </TableCell>
             </TableRow>
-            <TableRow>
-              <TableCell colSpan={5} className="font-bold">
+
+            <TableRow className="bg-gray-300">
+              <TableCell className="font-bold md:hidden" colSpan={3}>
+                Other (Discretionary) - 20%
+              </TableCell>
+              <TableCell className="hidden font-bold md:table-cell" colSpan={6}>
                 Other (Discretionary) - 20%
               </TableCell>
             </TableRow>
-            <TableRow>
-              <TableCell className="font-bold">Total</TableCell>
-              <TableCell className="font-bold">100%</TableCell>
-              <TableCell className="font-bold">$total</TableCell>
-              <TableCell className="font-bold">$total</TableCell>
+
+            <TableRow className="bg-gray-200">
+              <TableCell className="category font-bold">Total</TableCell>
+              <TableCell className="percentage hidden font-bold md:table-cell">
+                %
+              </TableCell>
+              <TableCell className="budgeted hidden font-bold md:table-cell">
+                B
+              </TableCell>
+              <TableCell className="spent hidden font-bold md:table-cell">
+                S
+              </TableCell>
+              <TableCell className="remaining font-bold">R</TableCell>
+              <TableCell className="trash" align="right"></TableCell>
             </TableRow>
           </TableBody>
         </Table>
--- src/components/tables/transactionsTables.tsx
+++ src/components/tables/transactionsTables.tsx
@@ -1,31 +1,77 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function TransactionsTable() {
-	return (
-		<div className="transactions-table-wrapper">
-			<TableContainer component={Paper} className="table-wrapper my-4">
-				<Table aria-label="Transactions Table" size="small" id="transactions-table">
-					<TableHead>
-						<TableRow>
-							<TableCell colSpan={4} className="font-bold">Transactions</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className="font-bold">Date</TableCell>
-							<TableCell className="font-bold">Category</TableCell>
-							<TableCell className="font-bold">Description</TableCell>
-							<TableCell className="font-bold">Amount</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						<TableRow>
-							<TableCell>9/12/2025</TableCell>
-							<TableCell>Savings Contributions</TableCell>
-							<TableCell>Savings Acct</TableCell>
-							<TableCell>$50</TableCell>
-						</TableRow>
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</div>
-	)
+  return (
+    <div className="transactions-table-wrapper">
+      <TableContainer component={Paper} className="table-wrapper my-4">
+        <Table
+          aria-label="Transactions Table"
+          size="small"
+          id="transactions-table"
+        >
+          <TableHead>
+            <TableRow className="bg-gray-200 border-b-2 border-gray-300">
+              <TableCell colSpan={2} className="font-bold md:hidden">
+                Transactions
+              </TableCell>
+              <TableCell colSpan={4} className="hidden font-bold md:table-cell">
+                Transactions
+              </TableCell>
+              <TableCell align="right">
+                <IconButton size="small">
+                  <AddIcon fontSize="small" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+            <TableRow className="bg-gray-200">
+              <TableCell className="date hidden font-bold md:table-cell">
+                Date
+              </TableCell>
+              <TableCell className="category hidden font-bold md:table-cell">
+                Category
+              </TableCell>
+              <TableCell className="description font-bold">
+                Description
+              </TableCell>
+              <TableCell className="amount font-bold">Amount</TableCell>
+              <TableCell className="trash"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+
+            {/* TODO: map here */}
+            <TableRow>
+              <TableCell className="date hidden md:table-cell">
+                9/12/2025
+              </TableCell>
+              <TableCell className="category hidden md:table-cell">
+                Savings Contributions
+              </TableCell>
+              <TableCell className="description">Savings Acct</TableCell>
+              <TableCell className="amount">$50</TableCell>
+              <TableCell align="right" className="trash">
+                <IconButton size="small">
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
